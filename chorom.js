8d967d2118561a93b35e520ed16abe80e036906e
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-06-28 20:20:41 +0600

    done

--- chorom.js
+++ chorom.js
@@ -1,5 +1,20 @@
+/* step 1: name the value or create variable name
+   step 2: set input value and button click 
+            a. add event listener
+            b. push value
+            c. clear value when click 
+            d. set local storage JSON stringify
+    step 3: add list when input value is save
+            a. create function array
+            b. create loop by taking innerHTML
+    step 4: sting to array convert
+    step 5: set delete button
+    step 6: set tab button or link button 
+   
+   */
+            
 
-/*step one (take variable) */
+/* 1 step one (take variable) */
 let allLink =[];
 const input = document.getElementById("input");
 const inputBtn = document.getElementById("inputBtn");
@@ -7,25 +22,53 @@ const tabBtn = document.getElementById("tabBtn");
 const deleteBtn = document.getElementById("deleteBtn");
 const listItem = document.getElementById("list");
 
+
 //  console.log(input, inputBtn, tabBtn, deleteBtn, list)
 
-/*step two ( work input and input value) */
-inputBtn.addEventListener("click", () => {  // call function//
-    let links = input.value ; // take input value//
-    allLink.push(links); // take input value for push //
-    input.value =""; // to show empty the input value //
-    localStorage.setItem("links", allLink); // set local storage//
+/* 2 step two ( work input and input value) */
+inputBtn.addEventListener("click", () => {       //1 call function//
+    let links = input.value ;    //2 take input value//
+    allLink.push(links);     //3 take input value for push //
+    input.value ="";     //4 to show clear the input value //
+    localStorage.setItem("links", /*allLink*/ JSON.stringify(allLink));      // 5 set local storage//
 
-
-
-    renderArr(allLink); //step three //
+    renderArr(allLink); //from step 3 //
 });
 
-/*step three ( take array function, take loop, and attach with list) */
-
-function renderArr(arr){   // taking function//
-    arr.forEach((a) => {   // taking loop//
-        listItem.innerHTML += `  
-        <li><a href=${a} target="_blank" class="link">${a}</a></li>`;               // set inner HTML//
-    });
+/* 4 step four (string to array)*/
+let getLinksLocalStorage = JSON.parse(localStorage.getItem("links")); //from step 2 // 
+if (getLinksLocalStorage){  // convert if else//
+    allLink = getLinksLocalStorage;
+    renderArr(allLink);
 }
+
+/* 3 step three ( take array function, take loop, and attach with list) */
+function renderArr(arr){  //1 taking function//
+    listItem.innerHTML =""; //2 to listing on sigle line//
+
+    arr.forEach((a) => {   //3 taking loop//
+        listItem.innerHTML += `  
+        <li><a href=${a} target="_blank" class="link">${a}</a></li>`;    //4 set inner HTML//
+    });
+    
+}
+
+/* 5 step Four (work delete button)*/
+deleteBtn.addEventListener("click", () =>{
+    allLink = [];
+     localStorage.clear(); // to clear localstorage//
+    renderArr(allLink);
+})
+
+/* 6 step Five (tab button)*/
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0].url;
+        allLink.push(activeTab);
+        localStorage.setItem(links, JSON.stringify(allLink));
+        renderArr(allLink);
+      });
+})
+
+
+
